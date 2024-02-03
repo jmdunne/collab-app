6327abf8c3837f1d0529bff6b0a547b824e12fd8
Josh Dunne <joshua.m.dunne@gmail.com>
2024-02-02 17:29:11 -0700

    DB and Auth: Fixed the CSS to center the logo

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Loading = () => {
   return (
-    <div className="h-full w-full flex flex-col gap-y-4 items-center">
+    <div className="h-full w-full flex flex-col justify-center items-center">
       <Image
         src="/logo.svg"
         alt="logo"
