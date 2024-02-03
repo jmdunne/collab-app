6ebc7a52f4caad662fe5e019e847544949caa0c8
Josh Dunne <joshua.m.dunne@gmail.com>
2024-02-02 17:25:22 -0700

    DB and Auth: Add UserButton to show if logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>This is a screen for authenticated users only</div>
+      <div>
+        <UserButton />
+      </div>
+    </div>
+  );
 }
