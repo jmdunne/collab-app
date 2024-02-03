c9798d00e543d7e390c84d1774da940a61fe5754
Josh Dunne <joshua.m.dunne@gmail.com>
2024-02-02 17:12:24 -0700

    DB & Auth: Bug fix to get logo to load

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,7 +4,7 @@ export const Loading = () => {
   return (
     <div className="h-full w-full flex flex-col gap-y-4 items-center">
       <Image
-        src="/logo.svg "
+        src="/logo.svg"
         alt="logo"
         width={120}
         height={120}
