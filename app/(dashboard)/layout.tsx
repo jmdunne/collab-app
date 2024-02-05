d5a7162f5660d7cf62c1b1ed5afaa1e2c8fc075d
Josh Dunne <joshua.m.dunne@gmail.com>
2024-02-05 16:44:48 -0700

    DB LO: Add sb container, add spacing in LO, orgsb

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { OrgSidebar } from "./_components/org-sidebar";
 import { Sidebar } from "./_components/sidebar";
 
 interface DashboardLayoutProps {
@@ -8,7 +9,15 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <main className="h-full">
       <Sidebar />
-      <div className="pl-[60px] h-full">{children}</div>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            {/* Add Navbar here */}
+            {children}
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
