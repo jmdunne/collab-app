9f5c9d7e5fefe3b601aaa26325ba1987a4988136
Josh Dunne <joshua.m.dunne@gmail.com>
2024-02-21 16:30:30 -0700

    SB: Added Hint to items

--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -6,6 +6,8 @@ import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 import { cn } from "@/lib/utils";
 import { on } from "events";
 
+import { Hint } from "../hint";
+
 interface ItemProps {
   id: string;
   name: string;
@@ -26,17 +28,19 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
 
   return (
     <div className="aspect-square relative">
-      <Image
-        fill
-        alt={name}
-        src={imageUrl}
-        onClick={onClick}
-        className={cn(
-          "rounded-md",
-          "cursor-pointer opacity-75 hover:opacity-100 transition",
-          isActive && "opacity-100"
-        )}
-      />
+      <Hint label={name} side="right" align="start" sideOffset={18}>
+        <Image
+          fill
+          alt={name}
+          src={imageUrl}
+          onClick={onClick}
+          className={cn(
+            "rounded-md",
+            "cursor-pointer opacity-75 hover:opacity-100 transition",
+            isActive && "opacity-100"
+          )}
+        />
+      </Hint>
     </div>
   );
 };
