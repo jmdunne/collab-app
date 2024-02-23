3f254538a10b597dfc23aadeeffc9c940e927182
Josh Dunne <joshua.m.dunne@gmail.com>
2024-02-23 12:27:20 -0700

    Org SB: Add logo and title to org sb

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,27 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
-      Org sidebar
+    <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image src="/logo.svg" width={60} height={60} alt="Logo" />
+          <span className={cn("font-semibold text-2xl", font.className)}>
+            Collab
+          </span>
+        </div>
+      </Link>
     </div>
   );
 };
